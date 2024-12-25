9004d007ec75f5a248eb153985a15765c35e0980
Devansh Kushwaha <127891968+Devansh-Kushwaha@users.noreply.github.com>
2024-12-25 17:45:47 +0530

    Added Comments
    Added Comments to increase readbility and understandability

--- Array operations.c
+++ Array operations.c
@@ -1,102 +1,130 @@
 #include<conio.h>
 #include<stdio.h>
+
+// Define an array to store elements
 int ar[50];
-void create(n){
-    int i,a;
-    for(i=0;i<n;i++){
-        printf("Enter the number to be stored at %d= ",i);
-        scanf("%d",&a);
-        ar[i]=a;}
+
+// Function to create the array
+void create(int n) {
+    int i, a;
+    for (i = 0; i < n; i++) {
+        printf("Enter the number to be stored at index %d: ", i);
+        scanf("%d", &a);
+        ar[i] = a;  // Store the input number in the array
+    }
 }
-void display(n){
-    int i,a;
-    printf("Array- ");
-    for(i=0;i<n;i++){
-            printf("%d ",ar[i]);}
+
+// Function to display the elements of the array
+void display(int n) {
+    int i;
+    printf("Array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", ar[i]);  // Print each element of the array
+    }
 }
-void delet(int n){ //deletion//
-    int ch,i,v,j,ne;
-    printf("Which node do you wish to delete \n 1)Beginning node \n 2)Ending node \n 3)A Particular node \n");
-    scanf("%d",&ch);
-    switch(ch){
-        case 1:
-            for(i=1;i<=n;i++){
-                ar[i-1]=ar[i];}
-        break;
-        case 2:
-            ar[n-1]=NULL;
+
+// Function to delete an element from the array
+void delet(int n) {
+    int ch, i, j, ne;
+    printf("Which node do you wish to delete? \n 1) Beginning node \n 2) Ending node \n 3) A Particular node \n");
+    scanf("%d", &ch);
+    switch (ch) {
+        case 1:  // Deletion from the beginning
+            for (i = 1; i < n; i++) {
+                ar[i - 1] = ar[i];  // Shift elements to the left
+            }
             break;
-        case 3:
+        case 2:  // Deletion from the end
+            ar[n - 1] = 0;  // Remove the last element (set to 0)
+            break;
+        case 3:  // Deletion of a particular value
             printf("Which value do you wish to delete: ");
-            scanf("%d",&j);
-            for (i=0;i<n;i++){
-                if (ar[i]==j){
-                   for(ne=i;ne<n;ne++){
-                        ar[ne]=ar[ne+1];
-}}}}}
-void traverse(n){ //traversal//
-    int x=0;
-    printf("Now the linked list is: \n");
-while (x<n){
-    printf("Index number %d- %d \n ",x,ar[x]);
-    x++;}}
-
-void insert(int n){ //insertion//
-
-    int ch,i,v,l,j;
-    printf("Where do you want to insert a node \n 1)In the beginning 2)In the ending 3)After a node \n");
-    scanf("%d",&ch);
-    switch(ch){
-        case 1:
-            printf("Enter the value: ");
-            scanf("%d",&v);
-           for (i=n;i>0;i--){
-            ar[i+1]=ar[i];}
-           ar[0]=v;
-            break;
-        case 2:
-            printf("Enter the value: ");
-            scanf("%d",&v);
-            ar[n+1]=v;
-            break;
-        case 3:
-            printf("After what value do you wish to add: ");
-            scanf("%d",&j);
-            printf("Enter the value: ");
-            scanf("%d",&v);
-            for (i=0;i<n;i++){
-                    if (j==ar[i]){
-                        for (l=n;l>i;l--){
-                            ar[l+1]=ar[l];}
-                        ar[i+1]=v;
+            scanf("%d", &j);
+            for (i = 0; i < n; i++) {
+                if (ar[i] == j) {
+                    for (ne = i; ne < n - 1; ne++) {
+                        ar[ne] = ar[ne + 1];  // Shift elements to the left
                     }
-}}}
-void main(){
-    int l,c;
-    printf("Enter the number of elements to be added in the Array");
-    scanf("%d",&l);
-    create(l);
-    display(l);
-    printf("\n");
-        int p=0;
-         while(p==0){
-    printf("What operation do you wish to perform: \n 1)Insertion \n 2)Deletion \n 3)Traversal \n 4)End program \n");
-    scanf("%d",&c);
-    switch(c){
-        case 1:
-            insert(l);
-            l++;
-            display(l);
-            break;
-        case 2:
-            delet(l);
-             l--;
-            display(l);
-            break;
-        case 3:
-            traverse(l);
-            break;
-        case 4:
-            p=1;}}
-   }
+                    break;
+                }
+            }
+    }
+}
 
+// Function to traverse the array and print its elements
+void traverse(int n) {
+    int x = 0;
+    printf("Now the array is: \n");
+    while (x < n) {
+        printf("Index %d: %d\n", x, ar[x]);
+        x++;
+    }
+}
+
+// Function to insert an element into the array
+void insert(int n) {
+    int ch, i, v, l, j;
+    printf("Where do you want to insert a node? \n 1) In the beginning \n 2) At the end \n 3) After a node \n");
+    scanf("%d", &ch);
+    switch (ch) {
+        case 1:  // Insertion at the beginning
+            printf("Enter the value: ");
+            scanf("%d", &v);
+            for (i = n; i > 0; i--) {
+                ar[i] = ar[i - 1];  // Shift elements to the right
+            }
+            ar[0] = v;  // Insert the new value at index 0
+            break;
+        case 2:  // Insertion at the end
+            printf("Enter the value: ");
+            scanf("%d", &v);
+            ar[n] = v;  // Add the value at the end of the array
+            break;
+        case 3:  // Insertion after a specific value
+            printf("After what value do you wish to add: ");
+            scanf("%d", &j);
+            printf("Enter the value: ");
+            scanf("%d", &v);
+            for (i = 0; i < n; i++) {
+                if (ar[i] == j) {
+                    for (l = n; l > i; l--) {
+                        ar[l] = ar[l - 1];  // Shift elements to the right
+                    }
+                    ar[i + 1] = v;  // Insert the new value after the specified value
+                    break;
+                }
+            }
+    }
+}
+
+// Main function to perform various operations on the array
+void main() {
+    int l, c;
+    printf("Enter the number of elements to be added in the array: ");
+    scanf("%d", &l);
+    create(l);  // Initialize the array with elements
+    display(l); // Display the array
+    printf("\n");
+    int p = 0;
+    while (p == 0) {  // Menu-driven operations
+        printf("What operation do you wish to perform? \n 1) Insertion \n 2) Deletion \n 3) Traversal \n 4) End program \n");
+        scanf("%d", &c);
+        switch (c) {
+            case 1:
+                insert(l);  // Insert an element
+                l++;  // Increase the size of the array
+                display(l); // Display the updated array
+                break;
+            case 2:
+                delet(l);  // Delete an element
+                l--;  // Decrease the size of the array
+                display(l); // Display the updated array
+                break;
+            case 3:
+                traverse(l);  // Traverse and print the array
+                break;
+            case 4:
+                p = 1;  // Exit the loop and end the program
+        }
+    }
+}
